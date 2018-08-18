6441ab9b2639b10608e8b95e6a68bc71791e4e34
Mikhail <mvorobyev1782@gmail.com>
2018-08-18 20:18:30 +0300

    HW6: MapUuidStorage add new key

--- src/ru/javawebinar/basejava/storage/MapUuidStorage.java
+++ src/ru/javawebinar/basejava/storage/MapUuidStorage.java
@@ -5,8 +5,8 @@ import ru.javawebinar.basejava.model.Resume;
 import java.util.*;
 
 public class MapUuidStorage extends AbstractStorage {
-    private final Map<String, Resume> storage = new HashMap<>(); //key for Map - uuid
-
+    //private final Map<String, Resume> storage = new HashMap<>(); //key for Map - uuid
+    private final Map<Resume, Resume> storage = new HashMap<>(); //key for Map - Resume
     @Override
     public void clear() {
         storage.clear();
@@ -19,21 +19,25 @@ public class MapUuidStorage extends AbstractStorage {
 
     @Override
     public void saveElement(Resume resume, Object elementKey) {
-        storage.put((String) elementKey, resume);
+        //storage.put((String) elementKey, resume);
+        storage.put(resume, resume);
     }
 
     @Override
     public void deleteElement(Object elementKey) {
+        //storage.remove(elementKey);
         storage.remove(elementKey);
     }
 
     @Override
     public void updateElement(Resume resume, Object elementKey) {
-        storage.put((String) elementKey, resume);
+        //storage.put((String) elementKey, resume);
+        storage.put(resume, resume);
     }
 
     @Override
     public Resume getElement(Object elementKey) {
+        //return storage.get(elementKey);
         return storage.get(elementKey);
     }
 
@@ -46,11 +50,17 @@ public class MapUuidStorage extends AbstractStorage {
 
     @Override
     protected boolean containsElement(Object elementKey) {
+        //return storage.containsKey(elementKey);
         return storage.containsKey(elementKey);
     }
 
-    @Override
+    /*@Override
     protected String findElementKey(String uuid) {
         return uuid;
+    }*/
+
+    @Override
+    protected Resume findElementKey(String uuid) {
+        return new Resume(uuid);
     }
 }
