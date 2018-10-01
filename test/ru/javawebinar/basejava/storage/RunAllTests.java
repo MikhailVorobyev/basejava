65bf08049256bbac93840594228a9efad7578d81
Mikhail <mvorobyev1782@gmail.com>
2018-10-01 22:25:12 +0300

    Lesson10 Json serialization.

--- test/ru/javawebinar/basejava/storage/RunAllTests.java
+++ test/ru/javawebinar/basejava/storage/RunAllTests.java
@@ -11,7 +11,9 @@ import org.junit.runners.Suite;
     MapSearchKeyUuidStorageTest.class,
     MapSearchKeyResumeStorageTest.class,
     ObjectFileStorageTest.class,
-    ObjectPathStorageTest.class
+    ObjectPathStorageTest.class,
+    XmlPathStorageTest.class,
+    JsonPathStorageTest.class
 })
 public class RunAllTests {
 }
