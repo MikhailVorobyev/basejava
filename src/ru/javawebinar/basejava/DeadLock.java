2c1474122b4f953d497ca18af9d39422c626203b
Mikhail <mvorobyev1782@gmail.com>
2018-10-17 01:19:51 +0300

    HW11: simplified deadlock.

--- src/ru/javawebinar/basejava/DeadLock.java
+++ src/ru/javawebinar/basejava/DeadLock.java
@@ -1,43 +1,28 @@
 package ru.javawebinar.basejava;
 
 public class DeadLock {
-    private final Object lock1 = new Object();
-    private final Object lock2 = new Object();
+    private static final Object LOCK_1 = new Object();
+    private static final Object LOCK_2 = new Object();
 
-    public void method1() {
-        synchronized (lock1) {
-            System.out.println(Thread.currentThread() + " inside lock1");
+    public void deadLockMethod(Object firstLock, Object secondLock) {
+        synchronized (firstLock) {
+            System.out.println(Thread.currentThread() + " inside first lock");
             try {
                 Thread.sleep(50);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            System.out.println(Thread.currentThread() + " waiting for lock2...");
-            synchronized (lock2) {
-                System.out.println(Thread.currentThread() + " inside lock2");
-            }
-        }
-    }
-
-    public void method2() {
-        synchronized (lock2) {
-            System.out.println(Thread.currentThread() + " inside lock2");
-            try {
-                Thread.sleep(50);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            System.out.println(Thread.currentThread() + " waiting for lock1...");
-            synchronized (lock1) {
-                System.out.println(Thread.currentThread() + " inside lock1");
+            System.out.println(Thread.currentThread() + " waiting for second lock...");
+            synchronized (secondLock) {
+                System.out.println(Thread.currentThread() + " inside second lock");
             }
         }
     }
 
     public static void main(String[] args) {
         DeadLock deadLock = new DeadLock();
-        new Thread(deadLock::method1).start();
-        new Thread(deadLock::method2).start();
+        new Thread(() -> deadLock.deadLockMethod(LOCK_1, LOCK_2)).start();
+        new Thread(() -> deadLock.deadLockMethod(LOCK_2, LOCK_1)).start();
     }
 
 }
